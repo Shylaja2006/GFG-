e3df330ecaa7b02111e6ccfea15a406c8420b2ab
Shylaja2006 <shylushylaja399@gmail.com>
2026-03-17 23:46:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates from an array/remove-duplicates-from-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    ArrayList<Integer> remDuplicate(int arr[]) {
+        // code here
+        Arrays.sort(arr);
+        ArrayList<Integer> d=new ArrayList<>();
+        int i=0;
+        for(int j=1; j<arr.length; j++){
+            if(arr[i]!=arr[j]){
+                i++;
+             arr[i]=arr[j];
+            }
+        }
+            for(int k=0; k<=i; k++){
+                d.add(arr[k]);
+            }
+            return d;
+    
+}
+}
