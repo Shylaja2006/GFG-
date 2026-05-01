4c07f4d0fa43c64b7b1e159e49d05e3c777c434d
Shylaja2006 <shylushylaja399@gmail.com>
2026-05-01 19:31:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    static int sumOfNaturals(int n) {
+        // code here
+        int sum=n*(n+1)/2;
+        /*for(int i=0; i<=n; i++){
+            sum+=i;
+        }*/
+        return sum;
+    }
+}
